6b0829564895cd905bf3924b1a266882a37e615e
Rob Smith <sleepingkingstudios@gmail.com>
2024-03-15 07:02:01 -0400

    Implement generateUrl helper.

---
+++ src/form-api/utils.js
@@ -0,0 +1,69 @@
+/** @module form-api/utils */
+
+const applyWildcards = ({ url, wildcards = {} }) => {
+  let expected =
+    url.split('/').filter(s => s[0] === ':').map(s => s.slice(1));
+  let applied = url;
+
+  Object
+    .entries(wildcards)
+    .forEach(([wildcard, value]) => {
+      if (expected.indexOf(wildcard) === -1) {
+        throw new Error(`unknown wildcard "${wildcard}"`)
+      }
+
+      applied = applied.replace(
+        new RegExp(`:${wildcard}`, 'g'),
+        value.toString()
+      );
+      expected = expected.filter(item => item !== wildcard);
+    });
+
+  if (expected.length !== 0) {
+    throw new Error(`missing wildcard "${expected[0]}"`)
+  }
+
+  return applied;
+};
+
+const extractWildcards = ({ url }) => (
+  url.split('/').filter(s => s[0] === ':').map(s => s.slice(1))
+);
+
+const mergeQueryParameters = ({ queryParameters }) => {
+  if (!queryParameters) { return ''; }
+
+  const merged =
+    Object
+    .entries(queryParameters)
+    .map(
+      ([parameter, value]) => encodeURI(`${parameter}=${value}`)
+    )
+    .join('&')
+
+  return `?${merged}`;
+};
+
+/**
+ * Generates a url with wildcard values and query parameters.
+ * @param {string} baseUrl - The base url for the API, e.g. 'http://www.example.com'.
+ * @param {string} endpoint - The relative url for the API, e.g. '/resources/:resourceId'.
+ *    Can include wildcard values (prefixed with a colon ":").
+ * @param {Object} [queryParameters] - Values used to generate the URl query string.
+ * @param {Object} [wildcards] - Values used to populate the endpoint wildcards.
+ * @returns {string} The generated url.
+ * @throws Will throw an error if there are missing wildcards in endpoint that are not present in wildcards.
+ * @throws Will throw an error if there are extra wildcards in wildcards that are not present in endpoint.
+ */
+export const generateUrl = ({
+  baseUrl,
+  endpoint,
+  queryParameters,
+  wildcards,
+}) => {
+  const scopedEndpoint = endpoint[0] == '/' ? endpoint : `/${endpoint}`;
+  const params = mergeQueryParameters({ queryParameters });
+  const url = `${baseUrl}${scopedEndpoint}${params}`;
+
+  return applyWildcards({ url, wildcards });
+};
---
+++ src/form-api/utils.test.js
@@ -0,0 +1,78 @@
+import { generateUrl } from './utils';
+
+describe('api utils', () => {
+  describe('generateUrl', () => {
+    const baseUrl  = 'http://www.example.com';
+    const endpoint = 'examples';
+
+    it('generates the url', () => {
+      const expected = `${baseUrl}/${endpoint}`;
+
+      expect(generateUrl({ baseUrl, endpoint })).toEqual(expected);
+    });
+
+    describe('with query parameters', () => {
+      const queryParameters = {
+        limit: 10,
+        offset: 20,
+        sort: 'asc',
+      };
+      const expectedParams = 'limit=10&offset=20&sort=asc';
+
+      it('generates the url with query parameters', () => {
+        const expected = `${baseUrl}/${endpoint}?${expectedParams}`;
+
+        expect(
+          generateUrl({ baseUrl, endpoint, queryParameters })
+        ).toEqual(expected);
+      });
+    });
+
+    describe('with extra wildcards', () => {
+      const wildcards = { foo: 'bar' };
+
+      it('throws an error', () => {
+        expect(() => generateUrl({ baseUrl, endpoint, wildcards }))
+          .toThrow('unknown wildcard "foo"');
+      });
+    });
+
+    describe('with an endpoint with wildcards', () => {
+      const endpoint = 'examples/:exampleId/details/:detailId';
+
+      it('throws an error', () => {
+        expect(() => generateUrl({ baseUrl, endpoint }))
+          .toThrow('missing wildcard "exampleId"');
+      });
+
+      describe('with expected wildcards', () => {
+        const wildcards = {
+          detailId: 1,
+          exampleId: 'test-example',
+        };
+
+        it('generates the url with wildcard values', () => {
+          const applied = 'examples/test-example/details/1';
+          const expected = `${baseUrl}/${applied}`;
+
+          expect(
+            generateUrl({ baseUrl, endpoint, wildcards })
+          ).toEqual(expected);
+        });
+      });
+
+      describe('with extra wildcards', () => {
+        const wildcards = {
+          detailId: 1,
+          exampleId: 'test-example',
+          foo: 'bar',
+        };
+
+        it('throws an error', () => {
+          expect(() => generateUrl({ baseUrl, endpoint, wildcards }))
+            .toThrow('unknown wildcard "foo"');
+        });
+      });
+    });
+  });
+});
